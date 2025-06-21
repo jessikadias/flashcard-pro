fd30c6fad34a22a230d72bbfb0b14cbf29435e73
Jéssika Dias <jessika.dias@unico.io>
2025-06-21 14:35:02 -0300

    refactor(components): Improve icon and dropdown components with flexible sizing

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,15 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+@props(['as' => 'a'])
+
+@php
+$classes = 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
+@endphp
+
+@if ($as === 'button')
+    <button {{ $attributes->merge(['type' => 'button', 'class' => $classes]) }}>
+        {{ $slot }}
+    </button>
+@else
+    <a {{ $attributes->merge(['class' => $classes]) }}>
+        {{ $slot }}
+    </a>
+@endif
