f2e3ffffe5b2ee51a0c075ffa1c709509695ec9e
Jéssika Dias <jessika.dias@unico.io>
2025-06-24 20:49:08 -0300

    refactor: consolidate layout and clean up Blade components
    - Change Livewire layout configuration from 'components.layouts.app' to 'layouts.app'.
    - Remove obsolete app.blade.php component and consolidate layout logic into layouts/app.blade.php.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,24 +20,24 @@
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            @if (isset($header))
+            @isset($header)
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endisset
 
             <!-- Page Content -->
             <main>
                 {{ $slot }}
             </main>
         </div>
-
-        @if (!auth()->user()->hasCompletedOnboarding())
-            <div id="onboarding-tutorial"></div>
-        @endif        
+        
+        @auth
+            <livewire:onboarding-tutorial />
+        @endauth
         
         @livewireScripts
     </body>
-</html>
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DeckController;
 use App\Livewire\DeckList;
 use App\Livewire\DeckDetails;
 
